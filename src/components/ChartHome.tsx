1f7d5c7be1e800c41d3714f55b4e344484dd67ef
CasFre4 <ocasfre@gmail.com>
2025-07-15 12:23:50 -0600

    redeploying

--- src/components/ChartHome.tsx
+++ src/components/ChartHome.tsx
@@ -2,20 +2,22 @@
 // import ChartMe from '../../../ChartMe'
 import ChartMe from 'chartme'
 // import ChartMe from 'chartme'
-import {useRef} from 'react'
+import {useEffect, useRef} from 'react'
 import chartdata from '../data/chartdata.json'
 
 
 function ChartReact() {
     const containerRef = useRef<HTMLDivElement>(null);
     const hasRun = useRef(false)
-    if (hasRun.current) return;
-    hasRun.current = true
-    if (!containerRef.current) return
-    const chart = new ChartMe({})
-    chart.loadJSON(chartdata)
-    // console.log(`this.processed: ${chart.processed}, this.width: ${chart.width}, this.height: ${chart.height}`)
-    chart.graph({container: containerRef.current, height: 350 - 20 - 40, width: 252 - 20 - 40})//height and width define size of chart
+    useEffect(() => {
+        if (hasRun.current) return;
+        hasRun.current = true
+        if (!containerRef.current) return
+        const chart = new ChartMe({})
+        chart.loadJSON(chartdata)
+        // console.log(`this.processed: ${chart.processed}, this.width: ${chart.width}, this.height: ${chart.height}`)
+        chart.graph({container: containerRef.current, height: 350 - 20 - 40, width: 252 - 20 - 40})//height and width define size of chart
+        }, []);
     return <div ref={containerRef}/>
 }
 export default ChartReact
